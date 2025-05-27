1b145b12921cf1e0ea234d180e968d184f13f621
gustavo-rs <insiide123@gmail.com>
2025-05-27 09:19:20 -0300

    fix: auth

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,11 +1,13 @@
 import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../utils/auth";
 import type { ReactElement } from "react";
+import { useUser } from "@/store/auth-store";
 
 interface Props {
   children: ReactElement;
 }
 
 export const PrivateRoute = ({ children }: Props) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  const { isAuthenticated } = useUser();
+  
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
